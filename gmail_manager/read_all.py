79804cea43cc17d956fe8dc343dbc088c442c7cd
maurapintor <maura.pintor@gmail.com>
2017-07-27 08:41:31 +0200

    inserita gestione mail con gmail library

--- gmail_manager/read_all.py
+++ gmail_manager/read_all.py
@@ -5,6 +5,10 @@ g.login("tesinasicurezza", "computersecurity")
 emails = g.inbox().mail()
 for e in emails:
 	e.fetch()
-	print e.body
+	print e.__dict__.keys()
 g.logout()
 
+# ['body', 'delivered_to', 'fr', 'uid', 'thread', 'to',
+#  'cc', 'labels', 'mailbox', 'attachments', 'headers',
+#  'html', 'flags', 'sent_at', 'message', 'subject',
+#  'thread_id', 'message_id', 'gmail']
